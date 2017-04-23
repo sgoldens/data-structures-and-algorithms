831ce93740ff16d3fc0542c31eb798fadba8f653
Sasha Goldenson <sgoldens@gmail.com>
2017-04-23 10:44:49 -0700

    Binary Search Tree, insert, search, and delete.

--- BinarySearchTree.rb
+++ BinarySearchTree.rb
@@ -10,9 +10,13 @@
 #
 # Test-Driven Development: Developed using ## unit tests with a total of ## assertions which describe and document this Binary Search Tree.
 #
-# Binary Search Trees are a special pattern of the Tree pattern. They must follow the algorithmic rules which say the left child of the parent is always lesser and
-# the right child is always greater than its parent. If there is already a child in the direction you must move while traversing the tree, you repeat the same rules until
-# an avaialble space is found. 
+# Binary Search Trees are a special pattern of the Tree pattern. They must follow the algorithmic rules which say the left child of the parent is always
+# lesser and the right child is always greater than its parent. If there is already a child in the direction you must move while traversing the tree, you
+# repeat the same rules until an avaialble space is found. 
+#
+# Because BSTs are sorted in this fashion, operations of insertion, searching, and deleting is done faster than the linear time operations done on an 
+# unsorted tree. Binary insert, search, and delete are completed in O(log n), "Logarmithmic" time, which is the fastest there is behind O(1), 
+# "Constant time".
 #
 ##########
 # Unit Tests
@@ -109,6 +113,7 @@ class BinarySearchTreeTest < Test::Unit::TestCase
   def test_binary_search_tree_remove_method_deletes_value
     test = BinarySearchTree.new()
     values = [4,9,141,48,7]
+
     values.each { |val| test.insert(val) }
 
     assert_equal(5, test.size)
@@ -123,4 +128,116 @@ class BinarySearchTreeTest < Test::Unit::TestCase
     assert_equal(true, test.search(141))
   end
 
+end
+
+#####################
+# Node class
+#####################
+class Node
+
+  attr_accessor :value, :left_child, :right_child
+
+  def initialize(value=nil)
+    @value = value
+    @left_child = nil
+    @right_child = nil
+  end
+
+end
+
+#####################
+# BinarySearchTree class
+#####################
+
+class BinarySearchTree
+
+  attr_accessor :root, :size
+
+  def initialize
+    @root = nil
+    @size = 0
+  end
+
+  def insert(value)
+    if @root.nil?
+      @root = Node.new(value)
+      @size += 1
+    else
+      findAndInsert = lambda do |current_node|
+        if value > current_node.value
+          if current_node.right_child.nil?
+            current_node.right_child = Node.new(value)
+          else
+            findAndInsert.call(current_node.right_child)
+          end
+        elsif value < current_node.value
+          if current_node.left_child.nil?
+            current_node.left_child = Node.new(value)
+          else
+            findAndInsert.call(current_node.left_child)
+          end
+        end
+      end
+      findAndInsert.call(@root)
+      @size += 1
+    end
+    p
+  end
+
+  # Binary search
+  # Time Complexity: O(logn)
+  # Auxiliary Space Complexity: O(1)
+  def search(target)
+    check = false
+    traverse = lambda do |current_node|
+      if current_node == nil
+        return
+      elsif current_node.value == target
+        check = true
+        return
+      end
+      if target > current_node.value 
+        traverse.call(current_node.right_child)
+      elsif target < current_node.value
+        traverse.call(current_node.left_child)
+      end
+    end
+    traverse.call(@root)
+    check
+  end
+
+  def remove(deleteValue)
+    temp = []
+
+    # recursively populate a temp array with the values of the BST, except for the deleteValue
+    roundUp = lambda do |current_node|
+      if current_node == nil
+        return
+      end
+
+      if current_node.value != deleteValue
+        temp.push(current_node.value)
+      end
+
+      roundUp.call(current_node.right_child)
+      roundUp.call(current_node.left_child)
+    end
+
+    roundUp.call(@root)
+
+    if temp.length === self.size
+      p "deleteValue $#{deleteValue} doesn't exist inside our tree."
+    end
+
+    # reset the BST by setting its root to nil and size to 0 
+    self.root = nil
+    self.size = 0
+
+    # repopulate and return the BST with the temp array values
+    temp.each { |val| self.insert(val) }
+  end
+
+  def method_missing(name, *args, &block)
+    @self.send(name, *args, &block)
+  end
 end
