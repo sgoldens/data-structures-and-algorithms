b65282ec1b6615b16c4826b3a2ff9c7d0b052932
Sasha Goldenson <sgoldens@gmail.com>
2017-04-26 22:48:58 -0700

    Update Graph BFS as primarily a traversal with a walked path output

--- GraphBFS.rb
+++ GraphBFS.rb
@@ -10,7 +10,7 @@
 # and the ending point is called the Terminal. Furthermore, for BFS, the starting point is sometimes called its 'search_key.'
 #
 # BFS needs a 'search_key' to run on its graph. Our BFS uses additional two data structures, an array as an adjacency list and 
-# a hash to keep track of which Vertices have already been visited. Our BFS prints the path walked and returns true if the 'search_key' is found, 
+# a hash to keep track of which Vertices have already been visited. Our BFS prints the path walked if the 'search_key' is found, 
 # or false if the 'search_key' does not exist in the graph.
 #
 #   Additional definitions of terms for Graph Theory (https://en.wikipedia.org/wiki/Graph_theory)
@@ -35,6 +35,7 @@
 ##########
 
 require_relative('Graph')
+require 'pp'
 
 class GraphBFSTest < Test::Unit::TestCase
   
@@ -42,16 +43,16 @@ class GraphBFSTest < Test::Unit::TestCase
     test = GraphBFS.new
     test.populate([[0,1], [0,2], [0,3], [1,4], [1,6], [1,8], [2,3], [2,7], [2,8], [3,1], [3,8], [4,6], [5,7], [5,8], [6,4], [6,9], [7,4], [8,1], [9,4]])
 
-    assert_equal(true, test.bfs(0))
-    assert_equal(true, test.bfs(1))
-    assert_equal(true, test.bfs(2))
-    assert_equal(true, test.bfs(3))
-    assert_equal(true, test.bfs(4))
-    assert_equal(true, test.bfs(5))
-    assert_equal(true, test.bfs(6))
-    assert_equal(true, test.bfs(7))
-    assert_equal(true, test.bfs(8))
-    assert_equal(true, test.bfs(9))
+    assert_equal([0, 1, 2, 3, 4, 6, 8, 7, 9], test.bfs(0))
+    assert_equal([1, 2, 3, 4, 6, 8, 7, 9], test.bfs(1))
+    assert_equal([2, 1, 3, 4, 6, 8, 7, 9], test.bfs(2))
+    assert_equal([3, 1, 2, 4, 6, 8, 7, 9], test.bfs(3))
+    assert_equal([4, 1, 2, 3, 6, 8, 7, 9], test.bfs(4))
+    assert_equal([5, 1, 2, 3, 4, 6, 8, 7, 9], test.bfs(5))
+    assert_equal([6, 1, 2, 3, 4, 8, 7, 9], test.bfs(6))
+    assert_equal([7, 1, 2, 3, 4, 6, 8, 9], test.bfs(7))
+    assert_equal([8, 1, 2, 3, 4, 6, 7, 9], test.bfs(8))
+    assert_equal([9, 1, 2, 3, 4, 6, 8, 7], test.bfs(9))
   end
 
   def test_graph_bfs_returns_true_when_target_is_not_found
@@ -74,38 +75,47 @@ class GraphBFS < Graph
     super
   end
 
- # Breadth First Search (BFS)
+ # Breadth First Search (BFS) graph traversal 
   def bfs(search_key)
+    # Track the traversal walked
+    walked = []
     # Create a FIFO queue using Array#push to enqueue and Array#shift to dequeue
     queue = Array.new
-    # Create a hash to track what's already been seen
-    visited = {}
-    # If the search_key exists in the graph
-    if get_vertex(search_key)
-      # Mark it as visited
-      visited[search_key] = true
-    end
-
-    # Build the adjacency list for this graph and feed it into the queue
+    # Build the adjacency list for this graph by feeding each Vertex's edges into the queue
     @vertices.each do |vertex|
       queue << find_neighbors(vertex[1].value)
     end
 
-    # Loop through the adjacency list
+    # Create a hash to track what's already been seen
+    visited = {}
+    # Check if the search_key exists as a starting point and
+    # If the search_key exists in the graph
+    if @vertices[search_key]
+      # Mark it as visited
+      visited[search_key] = true
+      # and add it as the first vertex visited
+      walked << search_key
+    end
+
+
+    # Loop through the adjacency list as a queue
     queue.each do |edges|
-      # and loop through each edge
+      # and loop through each adjacency list element as a collection of directed edges
       edges.each do |edge_to|
         # and when a directed edge goes to a previously unvisited Vertex
         if !visited[edge_to]
           # Mark that Vertex as now visited
           visited[edge_to] = true
+          # And add it as the next step of our BFS walk
+          walked << edge_to
         end
       end
     end
 
-    # If the search_key exists in the visited hash populated during graph BFS traversal
+    # Print out the walked BFS traversal path and return true if the search_key exists
     if visited[search_key] 
-      return true
+      return walked
+    # Else Print
     else
       return false
     end
