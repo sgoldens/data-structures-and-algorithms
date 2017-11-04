888132d65df6a9038bef3648fd2dc3ed85d31f63
Sasha Goldenson <sgoldens@gmail.com>
2017-11-03 17:07:45 -0700

    mergesort.rb

---
+++ algorithms/mergesort.rb
@@ -0,0 +1,24 @@
+# From wikipedia: https://en.wikibooks.org/wiki/Algorithm_Implementation/Sorting/Merge_sort#Ruby
+
+def mergesort(list)
+  return list if list.size <= 1
+  mid   = list.size / 2
+  left  = list[0, mid]
+  right = list[mid, list.size]
+  merge(mergesort(left), mergesort(right))
+end
+
+def merge(left, right)
+  sorted = []
+  until left.empty? || right.empty?
+    if left.first <= right.first
+      sorted << left.shift
+    else
+      sorted << right.shift
+    end
+  end
+  sorted.concat(left).concat(right)
+end
+
+test = Array.new(10) { |i| rand(100) }
+p (mergesort(test) === test.sort!) === true
