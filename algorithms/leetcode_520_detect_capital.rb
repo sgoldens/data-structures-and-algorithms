8604bcd77486b35421621e9ae2e89ff7d9b12919
Sasha Goldenson <sgoldens@gmail.com>
2017-11-13 20:16:38 -0800

    leetcode #520 - Detect Capital

---
+++ algorithms/leetcode_520_detect_capital.rb
@@ -0,0 +1,35 @@
+# https://leetcode.com/problems/detect-capital/description/
+# 520. Detect Capital
+
+# Given a word, you need to judge whether the usage of capitals in it is right or not.
+
+# We define the usage of capitals in a word to be right when one of the following cases holds:
+
+# All letters in this word are capitals, like "USA".
+# All letters in this word are not capitals, like "leetcode".
+# Only the first letter in this word is capital if it has more than one letter, like "Google".
+# Otherwise, we define that this word doesn't use capitals in a right way.
+# Example 1:
+# Input: "USA"
+# Output: True
+# Example 2:
+# Input: "FlaG"
+# Output: False
+# Note: The input will be a non-empty word consisting of uppercase and lowercase latin letters.
+
+# @param {String} word
+# @return {Boolean}
+def detect_capital_use(word)
+    return true if word.upcase === word
+    return true if word.downcase === word
+    return true if word.length > 1 && word.capitalize === word
+    return false
+end
+
+capital = "USA"
+flag = "FlaG"
+leetcode = "leetcode"
+
+p detect_capital_use(capital) === true
+p detect_capital_use(leetcode) === true
+p detect_capital_use(flag) === false
